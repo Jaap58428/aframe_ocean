eb756b7abe2c1b8bfe59273f5350dc565f8f4155
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-04-28 21:17:06 +0200

    can create ocean

--- waves.js
+++ waves.js
@@ -21,31 +21,37 @@ const createOcean = (oceanLength, oceanWidth, waveHeight, oceanResolution) => {
     // grab complete ocean to fill with elements
     let ocean = document.getElementById('ocean');
 
-    for (let i = 0; i < oceanLength; i++) {
-        for (let j = 0; j < oceanWidth; j++) {
+    for (let row = 0; row < oceanLength; row++) {
+        for (let column = 0; column < oceanWidth; column++) {
             let newOceanElement = getNewOceanElement();
             newOceanElement.setAttribute('color', getRandomBlueTint());
-            newOceanElement.setAttribute('position', getRandomBlueTint());
-            // add coordinates
+            newOceanElement.setAttribute('position', `
+                ${row * oceanResolution}, 
+                ${0 - waveHeight}, 
+                ${(0 - column) * oceanResolution}  // backwards: in front instead of backwards
+            `);
+            console.log(newOceanElement);
+            
             // add animation according to timing?
+            ocean.appendChild(newOceanElement);
         }        
     }
 }
 
 const getRandomBlueTint = () => {
     // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value#HSL_colors
-    const hue = (Math.random() * 60) + 180;
-    const saturation = (Math.random() * 50) + 25;
-    const lightness = (Math.random() * 50) + 25;
+    const hue = Math.round((Math.random() * 60)) + 180;
+    const saturation = Math.round((Math.random() * 50)) + 25;
+    const lightness = Math.round((Math.random() * 50)) + 25;
 
-    return `hsl(${hue}, ${saturation}, ${lightness}`;
+    return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
 
 const getNewOceanElement = () => {
-    let  = document.createElement('a-box');
-    oceanElement.setAttribute('depth', oceanResolution.toString);
-    oceanElement.setAttribute('width', oceanResolution.toString);
-    oceanElement.setAttribute('height', (waveHeight + 0.1).toString);
+    let oceanElement = document.createElement('a-box');
+    oceanElement.setAttribute('depth', oceanResolution.toString());
+    oceanElement.setAttribute('width', oceanResolution.toString());
+    oceanElement.setAttribute('height', (waveHeight + 0.1).toString());
     oceanElement.className = "oceanElement"
 
     return oceanElement;
