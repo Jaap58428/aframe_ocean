02f84739a5c700e4d834dea033ef3bf824112e2c
Jaap Kanbier <jaapkanbier@hotmail.com>
2019-04-28 22:05:11 +0200

    waves are real

--- waves.js
+++ waves.js
@@ -8,14 +8,14 @@
 
 
 // set global ocean variables
-const oceanLength = 10;  // how many elements long the ocean is
-const oceanWidth = 10;  // how many elements wide the ocean is
+const oceanLength = 15;  // how many elements long the ocean is
+const oceanWidth = 15;  // how many elements wide the ocean is
 const waveHeight = 1;  // how high each element can rise as part of a wave
 
 // how wide and deep every element is
 // smaller elements create finer defined ocean at the cost of computing
 // default = 0.25
-const oceanResolution = 0.25;  
+const oceanResolution = 0.5;  
 
 const createOcean = (oceanLength, oceanWidth, waveHeight, oceanResolution) => {
     // grab complete ocean to fill with elements
@@ -25,17 +25,43 @@ const createOcean = (oceanLength, oceanWidth, waveHeight, oceanResolution) => {
         for (let column = 0; column < oceanWidth; column++) {
             let newOceanElement = getNewOceanElement();
             newOceanElement.setAttribute('color', getRandomBlueTint());
-            newOceanElement.setAttribute('position', `
-                ${row * oceanResolution}, 
-                ${0 - waveHeight}, 
-                ${(0 - column) * oceanResolution}  // backwards: in front instead of backwards
-            `);
-            console.log(newOceanElement);
-            
-            // add animation according to timing?
+            newOceanElement.setAttribute('position', {
+                x: row * oceanResolution,
+                y: 0 - waveHeight,
+                z: (0 - column) * oceanResolution
+            });
+
+            newOceanElement.addEventListener('loaded', () => {
+                addAnimationToOceanElement(newOceanElement, row, column, waveHeight);
+            })   
+
             ocean.appendChild(newOceanElement);
         }        
     }
+}
+
+const addAnimationToOceanElement = (oceanElement, row, column, waveHeight) => {
+    let animation = document.createElement('a-animation');
+    animation.setAttribute('attribute', 'position');
+    animation.setAttribute('dur', '2000');
+
+    let currentLocation = oceanElement.getAttribute('position');
+    let newLocation = {
+        x: currentLocation.x, 
+        y: currentLocation.y + waveHeight, 
+        z: currentLocation.z
+    }
+    animation.setAttribute('from', `${currentLocation.x} ${currentLocation.y} ${currentLocation.z}`);
+    animation.setAttribute('to', `${newLocation.x} ${newLocation.y} ${newLocation.z}`);
+
+    animation.setAttribute('direction', 'alternate');
+    animation.setAttribute('easing', 'ease');
+    animation.setAttribute('repeat', 'indefinite');
+    animation.setAttribute('delay', '300');
+
+    setTimeout(() => {
+        oceanElement.appendChild(animation)
+    }, (row + column) * 1000);
 }
 
 const getRandomBlueTint = () => {
